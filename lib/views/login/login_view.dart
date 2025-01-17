323ef66d8ba406ed5d56064542166c0685dc6198
ddmuzyk <domino2k1@gmail.com>
2025-01-17 20:49:45 +0100

    add my_colors

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -1,7 +1,7 @@
+import 'package:dsw_52745/utils/my_colors.dart';
 import 'package:dsw_52745/utils/my_images.dart';
+import 'package:dsw_52745/views/register/register_view.dart';
 import 'package:flutter/material.dart';
-
-import '../register/register_view.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -14,11 +14,15 @@ class LoginView extends StatelessWidget {
           width: double.infinity,
           child: Column(
             children: [
-              SizedBox(height: 62),
+              const SizedBox(height: 62),
               Image.asset(MyImages.logo),
               Text(
                 'Sign in',
-                style: TextStyle(),
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                  color: MyColors.purpleColor,
+                ),
               ),
               ElevatedButton(
                 child: const Text('Open route'),
@@ -26,10 +30,18 @@ class LoginView extends StatelessWidget {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const RegisterView()),
+                        builder: (context) => const RegisterView(),),
                   );
                 },
               ),
+              GestureDetector(
+                child: const Text('Sign up'),
+                onTap: () {
+                  print('xxx1');
+                  print('xxx2');
+                },
+                onDoubleTap: () => print('clicked twice'),
+              )
             ],
           ),
         ),
